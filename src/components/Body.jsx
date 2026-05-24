ab2a24d262eb75cfcf9aad52909d0b79033adf55
Divas <divas.verma1956@gmail.com>
2026-05-24 16:48:46 +0530

    fetch profile on load; redirect to login if not logged in

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,43 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import NavBar from "./NavBar";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 function Body() {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    if (user) return;
+
+    try {
+      const res = await axios.get(`${BASE_URL}/profile/view`, {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        // means unauthorized request, only then navigate to login page, handle other error separately
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  // useEffect(() => {
+  //   if (!user) {
+  //     navigate("/login");
+  //   }
+  // }, [user]);
+
   return (
     <div>
       <NavBar />
