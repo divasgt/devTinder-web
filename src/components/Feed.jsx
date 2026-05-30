0df9a82e4cb06a1dffe5c937cb181471e0e81605
Divas <divas.verma1956@gmail.com>
2026-05-30 09:01:36 +0530

    feat and fix: handle feed loading, error, feed empty states

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,32 +2,47 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL } from "../utils/constants";
 import { addFeed } from "../utils/feedSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import UserCard from "./userCard";
 
 function Feed() {
   const feed = useSelector((store) => store.feed);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const dispatch = useDispatch();
 
-  const getFeed = async () => {
-    if (feed) return;
-
-    try {
-      const res = await axios.get(`${BASE_URL}/feed`, {
-        withCredentials: true,
-      });
-      dispatch(addFeed(res?.data?.data));
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
   useEffect(() => {
+    const getFeed = async () => {
+      if (feed) return;
+
+      try {
+        const res = await axios.get(`${BASE_URL}/feed`, {
+          withCredentials: true,
+        });
+        dispatch(addFeed(res?.data?.data));
+      } catch (err) {
+        console.error(err.message);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     getFeed();
   }, []);
 
   return (
-    <div>{feed && <UserCard user={feed[0]} className="mt-10 mx-auto" />}</div>
+    <div>
+      {loading ? (
+        <p className="mt-10 text-md text-center">Loading feed...</p>
+      ) : error ? (
+        <p className="mt-10 text-md text-center">Something went wrong</p>
+      ) : feed.length === 0 ? (
+        <p className="mt-10 text-md text-center">No new users found</p>
+      ) : (
+        <UserCard user={feed[0]} className="mt-10 mx-auto" />
+      )}
+    </div>
   );
 }
 
