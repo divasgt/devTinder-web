112724da8c66527c9432e414918c2a1d6d48df52
Divas <divas.verma1956@gmail.com>
2026-08-18 14:39:03 +0530

    feat & style: implement persistent chat messages in Chat page and improve style

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { createSocketConnection } from "../../utils/socket";
 import { useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../utils/constants";
 
 export default function Chat() {
   const { targetUserId } = useParams();
@@ -10,32 +12,57 @@ export default function Chat() {
   const userId = loggedInUser?._id;
   const [messageInput, setMessageInput] = useState("");
   const [messages, setMessages] = useState([]);
+  const socketRef = useRef(null); // Store single persistent socket instance
+
+  const fetchChatMessages = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/chat/${targetUserId}`, {
+        withCredentials: true,
+      });
+      const chat = res?.data?.data?.messages;
+      const chatMessages = chat.map((msg) => ({
+        firstName: msg.senderId.firstName,
+        text: msg.text,
+        createdAt: msg.createdAt,
+      }));
+
+      setMessages(chatMessages);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    if (!userId || !targetUserId) return;
+    fetchChatMessages();
+  }, [userId, targetUserId]);
 
   // on page load, socket connection is made and joinChat event is emitted
   useEffect(() => {
-    if (!userId) return;
+    if (!userId || !targetUserId) return;
 
     const socket = createSocketConnection();
+    socketRef.current = socket;
 
     socket.emit("joinChat", { firstName: loggedInUser.firstName, userId, targetUserId });
 
     socket.on("messageRecieved", ({ firstName, text }) => {
       console.log(`${firstName} sent message: ${text}`);
-      setMessages((prev) => [...prev, { firstName, text }]);
+      setMessages((prev) => [...prev, { firstName, text, createdAt: Date.now() }]);
     });
 
     return () => {
       // always disconnect socket
       socket.disconnect();
     };
-  }, [loggedInUser]);
+  }, [userId, targetUserId]);
 
   const sendMessage = (e) => {
     e.preventDefault();
-    if (messageInput === null || messageInput === "") return;
+    if (messageInput === null || messageInput.trim() === "") return;
 
-    const socket = createSocketConnection();
-    socket.emit("sendMessage", {
+    const socket = socketRef.current;
+    socket?.emit("sendMessage", {
       firstName: loggedInUser.firstName,
       userId,
       targetUserId,
@@ -54,15 +81,30 @@ export default function Chat() {
         </div>
 
         {/* Chat history */}
-        <div className="flex-1 space-y-2">
-          {messages.map((msg, index) => (
-            <div key={index} className={`flex flex-col items-start gap-0.5 ${""}`}>
-              <span className="text-xs text-fg-muted ml-1">{msg.firstName}</span>
-              <div className="px-3 py-1.5 bg-surface-2 inline-block border border-fg-muted/5 rounded-xl">
-                {msg.text}
+        <div className="flex-1 space-y-2 overflow-y-auto">
+          {messages.map((msg, index) => {
+            const isMe = msg.firstName === loggedInUser?.firstName;
+
+            return (
+              <div
+                key={index}
+                className={`flex flex-col gap-0.5 w-full ${isMe ? "items-end" : "items-start"}`}
+              >
+                <span className="text-[11px] text-fg-muted px-1">
+                  {isMe ? "You" : msg.firstName}
+                </span>
+                <div
+                  className={`px-3.5 py-2 text-sm max-w-[75%] break-words rounded-2xl ${
+                    isMe
+                      ? "bg-surface-2 border  border-fg-muted/10 rounded-br-xs"
+                      : "bg-primary rounded-bl-xs"
+                  }`}
+                >
+                  {msg.text}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
 
         {/* input box */}
@@ -72,7 +114,7 @@ export default function Chat() {
             value={messageInput}
             placeholder="Type a message"
             onChange={(e) => setMessageInput(e.target.value)}
-            className="input bg-surface-2 border-fg-muted/5"
+            className="input bg-surface-2 border-fg-muted/5 flex-1"
           />
           <button type="submit" className="btn-primary">
             Send
